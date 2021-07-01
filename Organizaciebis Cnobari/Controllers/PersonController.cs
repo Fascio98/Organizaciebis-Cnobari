630f3933740e1bdf83c74ceb0fee48c2f7105280
Nikoloz Bakhtadze <baxtura98@outlook.com>
2021-07-01 16:26:54 +0400

    Cvlilebebi, Kidev Erti!

--- Organizaciebis Cnobari/Controllers/PersonController.cs
+++ Organizaciebis Cnobari/Controllers/PersonController.cs
@@ -100,7 +100,20 @@ namespace Organizaciebis_Cnobari.Controllers
                 {
                     for (int i = 0; i < count; i++)
                     {
-                        if (_context.People.ToList()[i].PersonalID == model.PersonalID)
+                        if (_context.People.ToList()[i].PersonalID == model.PersonalID && _context.People.Where(x => x.Id == model.Id).FirstOrDefault().PersonalID==model.PersonalID)
+                        {
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().Id = model.Id;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().Name = model.Name;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().LastName = model.LastName;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().Gender = model.Gender;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().PersonalID = model.PersonalID;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().City = model.City;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().TelephoneNumbers = model.TelephoneNumbers;
+                            _context.People.Where(x => x.Id == model.Id).FirstOrDefault().Image = model.Image;
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction(nameof(PersonIndex));
+                        }
+                        if (_context.People.ToList()[i].PersonalID == model.PersonalID && _context.People.Where(x => x.Id == model.Id).FirstOrDefault().PersonalID != model.PersonalID)
                         {
                             ViewBag.Message = "ფიზიკური პირი ასეთი პირადი ნომერით უკვე დამატებულია!";
                             return View(model);
